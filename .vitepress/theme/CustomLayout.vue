e51d0f4c7c467ed589d06111ef9556fdd5830aae
Ligno We <ligno@bud-it.com>
2025-04-23 14:49:16 +0900

    게시글 표시 할 때 섬네일이 설정된 글의 경우 섬네일을 표시하도록 수정.

--- .vitepress/theme/CustomLayout.vue
+++ .vitepress/theme/CustomLayout.vue
@@ -1,10 +1,19 @@
 <template>
     <Layout>
         <template #doc-before>
-            <div class="post-meta">
-                <h1>{{ frontmatter.title }}</h1>
-                <p class="description">{{ frontmatter.description }}</p>
-                <p class="date">{{ formattedDate }}</p>
+            <div class="post-meta-wrapper">
+                <div class="post-meta">
+                    <h1>{{ frontmatter.title }}</h1>
+                    <p class="description">{{ frontmatter.description }}</p>
+                    <p class="date">{{ formattedDate }}</p>
+                </div>
+                <div v-if="frontmatter.thumbnail" class="thumbnail-container">
+                    <img
+                        :src="frontmatter.thumbnail"
+                        :alt="frontmatter.title"
+                        class="thumbnail-image"
+                    />
+                </div>
             </div>
         </template>
         <Content />
@@ -32,20 +41,45 @@ const formattedDate = computed(() => {
 </script>
 
 <style scoped>
-.post-meta {
+.post-meta-wrapper {
     margin-bottom: 2rem;
 }
+
+.post-meta {
+    padding: 1rem;
+    /* background-color: rgba(255, 255, 255, 0.8); */
+    border-radius: 8px;
+}
+
 .post-meta h1 {
     margin: 0;
 }
+
 .description {
     color: #7fa9d6;
     font-size: 0.9rem;
     margin: 0.5rem 0;
 }
+
 .date {
     color: #888;
     font-size: 0.8rem;
     text-align: right;
+}
+
+/* 섬네일 스타일 */
+.thumbnail-container {
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+    max-height: 200px;
+    margin-top: 1rem;
+}
+
+.thumbnail-image {
+    max-width: 100%;
+    height: auto;
+    object-fit: cover;
+    object-position: center;
 }
 </style>
